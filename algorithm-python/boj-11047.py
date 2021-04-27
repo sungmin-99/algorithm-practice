00df2907210b143bfe19cffaa2e8a8cf79ed2fa6
sungmin-99 <0411tjdals@naver.com>
2021-04-28 00:39:42 +0900

    initial commit

---
+++ algorithm-python/boj-11047.py
@@ -0,0 +1,12 @@
+import sys
+input = sys.stdin.readline
+n, k = map(int,input().split())
+money = []
+for _ in range(n):
+    money.append(int(input()))
+check = 0
+for i in range(n):
+    if k >= money[n-i-1]:
+        check += k // money[n-1-i]
+        k = k % money[n-1-i]
+print(check)
